19e823c9160e4b2caf47acd949f362ca5462e31a
YCLinYC <linyc.thomas@gmail.com>
2022-02-20 19:31:26 +0800

    added word breaking

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ from bs4 import BeautifulSoup
 from ckiptagger import WS
 from os import listdir
 
-def getArticle(url):
-
-
-    return none
-
-def hyphenation(article):
-
-
-    return none
-
 def main():
     #To use ckiptagger, you need to download this into the current directory
     if not('data' in listdir('./')):
@@ -20,7 +10,8 @@ def main():
         data_utils.download_data_gdown('./')
     
     #Visit google news to get all the articles
-    res = requests.get('https://news.google.com/topstories?hl=zh-TW&gl=TW&ceid=TW:zh-Hant')
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+    res = requests.get('https://news.google.com/topstories?hl=zh-TW&gl=TW&ceid=TW:zh-Hant', headers=headers)
     soup = BeautifulSoup(res.text, 'lxml')
     headlines = soup.select('h3 a') # select articles
 
@@ -48,22 +39,31 @@ def main():
         print(f'news{n+1:04d}\t{article_res.url}\t{media}\t{headline}\n', file=list_txt)
 
         #Find useful information within website
-        paragraph = article_soup.select('p')
+        paragraph = article_soup.select('p') #Select all <p>
         possible_div = {}
         for p in paragraph:
-            if len(p.text) > bias:
-                if possible_div.__contains__(p.findParent('p')):
-                    possible_div[p.findParent('p')] += 1
-                else:
-                    possible_div[p.findParent('p')] = 1
+            if len(p.text) > bias: #If one block of text has more words than bias
+                if possible_div.__contains__(p.findParent('div')): #If the block's parent div is already in dictionary
+                    possible_div[p.findParent('div')] += 1 
+                else: #If this is the first time the block's parent has been seen
+                    possible_div[p.findParent('div')] = 1
+        #I did this to filter out texts that are not useful
 
-        print(possible_div.values())
-        found_p = list(possible_div.keys())[list(possible_div.values()).index(max(possible_div.values()))]
-        target = found_p.select('p')
-        
-        for p in target:
-            if len(p.text) > bias and not("" in p.text):
-                print(p.text, file=f)
+        #Some website can't be scrapped, which results in not finding any div
+        if len(possible_div.values()) != 0:
+            found_p = list(possible_div.keys())[list(possible_div.values()).index(max(possible_div.values()))]
+            target = found_p.select('p')
+            
+            #Using CKIP to break off words
+            for p in target:
+                if len(p.text) > bias:
+                    ws = WS('./data')
+                    ws_results = ws([p.text])
+                    for w in ws_results:
+                        for ww in w:
+                            print(ww, end=' ', file=f)
+            
+            print(f'[{n+1}/{len(headlines)}]')
 
         f.close()
 
